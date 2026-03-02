b6c66b5d96e994a998ae9a07a787dce4e8fba107
mateoU1297 <castro.mateo@correounivalle.edu.co>
2026-03-02 07:46:13 -0500

    Initial commit

---
+++ src/main/java/com/pragma/restaurants/MsRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRestaurantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/restaurants/MsRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.restaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
